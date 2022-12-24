4faa1a59ef9a6a706a913f3fb927eb544b27ac41
eranner <104401464+eranner@users.noreply.github.com>
2022-12-23 20:18:14 -0500

    Add files via upload

---
+++ Scrabble/scrabble.js
@@ -0,0 +1,196 @@
+/*function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }*/
+  function shrinkA() {
+    var high = document.getElementById('a');
+    high.className = 'redo';
+  }
+
+var aShrink = document.getElementById('a');
+aShrink.addEventListener('click', shrinkA, false);
+
+  function shrinkB() {
+  var high = document.getElementById('b');
+  high.className = 'redo';
+}
+
+var bShrink = document.getElementById('b');
+bShrink.addEventListener('click', shrinkB, false);
+
+function shrinkC() {
+  var high = document.getElementById('c');
+  high.className = 'redo';
+}
+
+var cShrink = document.getElementById('c');
+cShrink.addEventListener('click', shrinkC, false);
+
+function shrinkD() {
+  var high = document.getElementById('d');
+  high.className = 'redo';
+}
+var dShrink = document.getElementById('d');
+dShrink.addEventListener('click', shrinkD, false);
+
+function shrinkE() {
+  var high = document.getElementById('e');
+  high.className = 'redo';
+}
+var eShrink = document.getElementById('e');
+eShrink.addEventListener('click', shrinkE, false);
+
+
+function shrinkF() {
+  var high = document.getElementById('f');
+  high.className = 'redo';
+}
+var fShrink = document.getElementById('f');
+fShrink.addEventListener('click', shrinkF, false);
+
+function shrinkG() {
+  var high = document.getElementById('g');
+  high.className = 'redo';
+}
+
+var gShrink = document.getElementById('g');
+gShrink.addEventListener('click', shrinkG, false);
+
+
+function shrinkH() {
+  var high = document.getElementById('h');
+  high.className = 'redo';
+}
+var hShrink = document.getElementById('h');
+hShrink.addEventListener('click', shrinkH, false);
+
+
+function shrinkI() {
+  var high = document.getElementById('i');
+  high.className = 'redo';
+}
+
+var iShrink = document.getElementById('i');
+iShrink.addEventListener('click', shrinkI, false);
+
+
+
+function shrinkJ() {
+  var high = document.getElementById('j');
+  high.className = 'redo';
+}
+
+var jShrink = document.getElementById('j');
+jShrink.addEventListener('click', shrinkJ, false);
+
+function shrinkK() {
+  var high = document.getElementById('k');
+  high.className = 'redo';
+}
+
+var kShrink = document.getElementById('k');
+kShrink.addEventListener('click', shrinkK, false);
+
+
+function shrinkL() {
+  var high = document.getElementById('l');
+  high.className = 'redo';
+}
+var lShrink = document.getElementById('l');
+lShrink.addEventListener('click', shrinkL, false);
+
+function shrinkM() {
+  var high = document.getElementById('m');
+  high.className = 'redo';
+}
+var mShrink = document.getElementById('m');
+mShrink.addEventListener('click', shrinkM, false);
+
+function shrinkN() {
+  var high = document.getElementById('n');
+  high.className = 'redo';
+}
+var nShrink = document.getElementById('n');
+nShrink.addEventListener('click', shrinkN, false);
+
+
+function shrinkO() {
+  var high = document.getElementById('o');
+  high.className = 'redo';
+}
+var oShrink = document.getElementById('o');
+oShrink.addEventListener('click', shrinkO, false);
+
+function shrinkP() {
+  var high = document.getElementById('p');
+  high.className = 'redo';
+}
+var pShrink = document.getElementById('p');
+pShrink.addEventListener('click', shrinkP, false);
+
+function shrinkQ() {
+  var high = document.getElementById('q');
+  high.className = 'redo';
+}
+
+var qShrink = document.getElementById('q');
+qShrink.addEventListener('click', shrinkQ, false);
+
+
+
+
+function shrinkR() {
+  var high = document.getElementById('r');
+  high.className = 'redo';
+}
+function shrinkS() {
+  var high = document.getElementById('s');
+  high.className = 'redo';
+}
+function shrinkT() {
+  var high = document.getElementById('t');
+  high.className = 'redo';
+}
+
+function shrinkU() {
+  var high = document.getElementById('u');
+  high.className = 'redo';
+}
+
+function shrinkV() {
+  var high = document.getElementById('v');
+  high.className = 'redo';
+}
+
+function shrinkW() {
+  var high = document.getElementById('w');
+  high.className = 'redo';
+}
+
+function shrinkX() {
+  var high = document.getElementById('x');
+  high.className = 'redo';
+}
+
+function shrinkY() {
+  var high = document.getElementById('y');
+  high.className = 'redo';
+}
+
+function shrinkZ() {
+  var high = document.getElementById('z');
+  high.className = 'redo';
+}
+function shrinkBlank() {
+  var high = document.getElementById('blank');
+  high.className = 'redo';
+}
---
+++ Scrabble/scrabbleBoard15x15.js
@@ -0,0 +1,95 @@
+function keyboard() {
+    document.getElementById('letters').style.opacity = '0'; 
+}
+
+function keyboardBack() {
+    document.getElementById('letters').style.opacity = '1';
+}
+const hide = document.getElementById('close');
+hide.addEventListener('click', keyboard, false);
+
+const reveal = document.getElementById('open');
+reveal.addEventListener('click', keyboardBack, false);
+
+
+const timer = document.getElementById('stopwatch');
+
+
+var sec = 60;
+var stoptime = true;
+
+function startTimer() {
+  if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
+    }
+}
+function stopTimer() {
+  if (stoptime == false) {
+    stoptime = true;
+  }
+}
+
+function timerCycle() {
+    if (stoptime == false) {
+    sec = parseInt(sec);
+    
+
+    sec = sec - 1;
+
+    if (sec == 0) {
+      
+    resetTimer();
+    }
+    
+    timer.innerHTML = sec;
+
+    setTimeout("timerCycle()", 1000);
+  }
+}
+
+function resetTimer() {
+    timer.innerHTML = '60';
+    stoptime = true;
+    
+    sec = 60;
+  
+}
+
+
+function allowDrop(ev) {
+    /* The default handling is to not allow dropping elements. */
+    /* Here we allow it by preventing the default behaviour. */
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    /* Here is specified what should be dragged. */
+    /* This data will be dropped at the place where the mouse button is released */
+    /* Here, we want to drag the element itself, so we set it's ID. */
+    ev.dataTransfer.setData("text/html", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data=ev.dataTransfer.getData("text/html");
+    /* If you use DOM manipulation functions, their default behaviour it not to 
+       copy but to alter and move elements. By appending a ".cloneNode(true)", 
+       you will not move the original element, but create a copy. */
+    var nodeCopy = document.getElementById(data).cloneNode(true);
+    nodeCopy.classList = "newId"; /* We cannot use the same ID */
+    ev.target.appendChild(nodeCopy);
+    nodeCopy.onclick = function(e) {this.parentNode.removeChild(this)}
+  }
+    ev.preventDefault(); {
+    /* In the drag event, we set the *variable* (it is not a variable name but a 
+       format, please check the reference!) "text/html", now we read it out */
+    var data=ev.dataTransfer.getData("text/html");
+    /* As we put the ID of the source element into this variable, we can now use 
+       this ID to manipulate the dragged element as we wish. */
+    /* Let's just move it through the DOM and append it here */
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+ 
